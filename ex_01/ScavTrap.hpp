eb43c4e8969cc8c5e536054ef5dd490a88f7abb8
Claudio Matheus <claudio1.code@gmail.com>
2026-03-11 17:27:31 -0300

    feat: implementar classes ClapTrap e ScavTrap com testes para ex01

---
+++ ex_01/ScavTrap.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clados-s <clados-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/11 15:54:55 by clados-s          #+#    #+#             */
+/*   Updated: 2026/03/11 16:40:08 by clados-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+class ScavTrap : public ClapTrap{
+	private:
+
+	public:
+		~ScavTrap();
+		ScavTrap();
+		ScavTrap(const ScavTrap& other);
+		ScavTrap(std::string name);
+		ScavTrap&	operator=(const ScavTrap& other);
+		void		attack(const std::string& target);
+};
+
+#endif
